d1ed71d8e02a7382d9d00a68ca4f3293dbacda31
Artem Mironov <31404136+artemko77788@users.noreply.github.com>
2022-02-03 19:30:06 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -8,51 +8,27 @@ const refs = {
   box: document.querySelector("#boxes"),
 };
 refs.buttonCreate.addEventListener("click", valueImput);
+refs.buttonDestroy.addEventListener("click", destroyBoxes);
 
 function valueImput() {
   let amount = document.querySelector("#controls input").value;
 
   createBoxes(amount);
 }
-
+let size = 30;
 function createBoxes(amount) {
-  const sizeBaseEl = 30;
-  // const div = document.createElement("div");
-  // div.style.cssText = `width: ${sizeBaseEl}px; height: ${sizeBaseEl}px; background-color: ${getRandomHexColor()}`;
-  // refs.box.appendChild(div);
-  // console.log("sadf");
   const fragment = document.createDocumentFragment();
+
   for (let i = 0; i < amount; i += 1) {
-    const size = sizeBaseEl + i * 10;
+    size += 10;
     const div = document.createElement("div");
     div.style.cssText = `width: ${size}px; height: ${size}px; background-color: ${getRandomHexColor()}`;
     fragment.appendChild(div);
   }
   refs.box.appendChild(fragment);
 }
-// const render = document.querySelector("[data-create]");
-// const destroy = document.querySelector("[data-destroy]");
-// const box = document.getElementById("boxes");
-// render.addEventListener("click", getAmount);
-// destroy.addEventListener("click", destroyBoxes);
 
-// function getAmount() {
-//   var amount = +document.querySelector("#controls input").value;
-//   createBoxes(amount);
-// }
-
-// function createBoxes(amount) {
-//   var basicSize = 30;
-//   var fragment = document.createDocumentFragment();
-//   for (var i = 0; i < amount; i++) {
-//     var size = basicSize + i * 10;
-//     var div = document.createElement("div");
-//     div.style.cssText = `width: ${size}px; height: ${size}px; background-color: ${getRandomHexColor()}`;
-//     fragment.appendChild(div);
-//   }
-//   box.appendChild(fragment);
-// }
-
-// function destroyBoxes() {
-//   box.innerHTML = "";
-// }
+function destroyBoxes() {
+  size = 30;
+  refs.box.innerHTML = "";
+}
